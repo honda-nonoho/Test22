dad44bfa8f40cd561a825f6f660f5d53b71292f8
honda-nonoho <nnhhnd3012@gmail.com>
2020-04-28 11:53:31 +0900

    直しましたよーっと。

---
+++ Test2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test2
+//
+//  Created by nonoho.honda on 2020/04/28.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
